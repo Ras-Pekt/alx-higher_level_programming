0c195b6a862aa0faff19bbc7478f9e3ddc780cdb
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-06-06 14:01:01 +0300

    print last number of integer

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-# YOUR CODE HERE
+num = number
+
+while (num > 0 and num > 10):
+    num %= 10
+
+while (num < 0 and num < -10):
+    num %= -10
+
+if (num > 5):
+    print(f"Last digit of {number} is {num} and is greater than 5")
+elif (num == 0):
+    print(f"Last digit of {number} is {num} and is 0")
+else:
+    print(f"Last digit of {number} is {num} and is less than 6 and not 0")
--- 0x01-python-if_else_loops_functions/README.md
+++ 0x01-python-if_else_loops_functions/README.md
@@ -1,4 +1,5 @@
 ## 0x01. Python - if/else, loops, functions
 This directory contains python programs/scripts that do the following:
 - [0-positive_or_negative.py](0-positive_or_negative.py) - prints if a number is 0, +ve or -ve
+- [1-last_digit.py](1-last_digit.py) - print the last number of an integer
 - 
