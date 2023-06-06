e6677b7c0f43529ff9a24e13f8e1a450c92d3222
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-06-06 19:04:23 +0300

    FizzBuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(1, 100):
+    for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz ", end="")
         elif i % 3 == 0:
@@ -10,4 +10,4 @@ def fizzbuzz():
         else:
             print("{} ".format(i), end="")
 
-    print("Buzz", end="")
+    print(" ", end="")
