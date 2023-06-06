f94b7871166cf77e2bdc40c37fd5221c56707064
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-06-06 22:46:35 +0300

    print alphabet in reverse

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+for i in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(i) if i % 2 == 0 else chr(i - 32)), end="")
+
+    """
+    if i % 2 == 0:
+        print(chr(i), end="")
+    else:
+        print(chr(i - 32), end="")
+    """
