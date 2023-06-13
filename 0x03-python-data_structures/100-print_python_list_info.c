e3da75022e8567b2306b52defcf8d2697bae238a
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-06-13 23:00:39 +0300

    print some basic info about Python lists

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,23 @@
+#include <Python.h>
+#include <stdio.h>
+/**
+ * print_python_list_info - prints some basic info about Python lists
+ * @p: python list
+ * Return: void
+ */
+void print_python_list_info(PyObject *p)
+{
+	long int idx = 0, size = PyList_Size(p);
+	PyObject *item;
+
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+
+	while (idx < size)
+	{
+		item = PyList_GET_ITEM(p, idx);
+		printf("Element %ld: %s\n", idx, Py_TYPE(item)->tp_name);
+		printf("Address of element %ld: %p\n", idx, (void *)item);
+		idx++;
+	}
+}
