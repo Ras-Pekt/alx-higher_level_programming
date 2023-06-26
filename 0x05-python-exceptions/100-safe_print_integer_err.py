3ff891596b82cec4ab81da0abb8909b167fd3d2d
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-06-26 17:13:36 +0300

    print an integer and write to stderr

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,8 +5,8 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except ValueError as verr:
-        print("Exception: {}".format(verr), file=sys.stderr)
+        print("Exception: {:s}".format(str(verr)), file=sys.stderr)
         return False
     except TypeError as terr:
-        print("Exception: {}".format(terr), file=sys.stderr)
+        print("Exception: {:s}".format(str(terr)), file=sys.stderr)
         return False
