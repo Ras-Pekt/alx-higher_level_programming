fb0358aebe051577ee32281253a34814a201bb83
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-06-26 15:26:28 +0300

    divide element by element 2 lists

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,7 +3,7 @@ def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     for i in range(list_length):
         try:
-            new_list.append(float(my_list_1[i]) / float(my_list_2[i]))
+            new_list.append(my_list_1[i] / my_list_2[i])
         except (ZeroDivisionError):
             new_list.append(0)
             print("division by 0")
