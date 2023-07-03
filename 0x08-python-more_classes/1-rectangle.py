768ef230a984ef176432700815c427a771afd881
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-07-03 14:02:12 +0300

    define rectangle with height and width

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,56 @@
+#!/usr/bin/pyhton3
+"""Define a rectangle class"""
+
+
+class Rectangle:
+    """Define a rectangle with width and height attributes"""
+    def __init__(self, width=0, height=0):
+        """initialize new rectangle
+        Args:
+            width (int): rectangle width
+            height (int): rectangle height
+        """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """get width"""
+        return self.__width
+
+    @property
+    def height(self):
+        """get height"""
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """set width
+        Args:
+	        value (int): rectangle width
+	    Raises:
+	        TypeError: if width is not int
+	        ValueError: if width < 0
+	    """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+    	    self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """set height
+        Args:
+            value (int): rectangle height
+        Raises:
+            TypeError: if height is not int
+            ValueError: if height < 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
--- 0x08-python-more_classes/README.md
+++ 0x08-python-more_classes/README.md
@@ -1,4 +1,8 @@
 ## 0x08. Python - More Classes and Objects
 This directory contains python programs/scripts that do the following:
 - [0-rectangle.py](0-rectangle.py) - an empty class Rectangle that defines a rectangle
+- [1-rectangle.py](1-rectangle.py) - defines a rectangle by:
+	- Private instance attribute: `width`
+	- Private instance attribute: `height`
+	- Instantiation with optional width and height: `def __init__(self, width=0, height=0)`
 - 
