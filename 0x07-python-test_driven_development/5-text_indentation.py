f01383b57d4ef4a8d488155b2d759f906f20f9c4
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-07-04 18:30:27 +0300

    matrix_divided tests

--- 0x07-python-test_driven_development/5-text_indentation.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""a function that prints a text with 2 new lines after each of these characters: ., ? and :"""
-
-
-def text_indentation(text):
-    if not isinstance(text, str):
-        raise TypeError("text must be a string")
-    
-    
