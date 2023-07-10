5a809fe8720f71d1e7db90b373c182240c08359b
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-07-10 22:28:38 +0300

    inverted behaviour

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,4 +1,4 @@
-#!/usr/bin/pyhton3
+#!/usr/bin/python3
 """MyInt class that inherits from int"""
 
 
@@ -6,12 +6,12 @@ class MyInt(int):
     """rebel class with inverted == and !="""
     def __eq__(self, other):
         """returns false if equal"""
-        if int.__eq__(self, other):
+        if super().__eq__(other):
             return False
         return True
 
     def __ne__(self, other):
         """returns false is not equal"""
-        if int.__ne__(self, other):
+        if super().__ne__(other):
             return False
         return True
