171752226c7e7e5b8108958c774a4873847c83cd
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-07-11 14:42:00 +0300

    append to file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,11 @@
+#!/usr/bin/pyhton3
+"""append a string to a file and returns count of characters appended"""
+
+
+def append_write(filename="", text=""):
+    """append string to a file
+    Returns:
+        count of characters appended
+    """
+    with open(filename, "a", encoding="utf-8") as fobj:
+        return (fobj.write(text))
