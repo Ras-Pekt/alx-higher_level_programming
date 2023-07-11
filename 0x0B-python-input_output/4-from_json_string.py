1b42cd38c737589f70897a6452a8efe85ac547e0
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-07-11 14:59:46 +0300

    rep str to json

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""returns an object represented by a JSON string"""
+import json
+
+
+def from_json_string(my_str):
+    """convert obj to json string"""
+    return (json.loads(my_str))
