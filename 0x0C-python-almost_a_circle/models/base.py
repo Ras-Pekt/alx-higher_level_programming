84ee5296b4d46cb469bbeae783ed825b398a7a43
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-08-04 16:01:49 +0300

    correct

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -14,6 +14,7 @@ class Base:
         else:
             self.id = id
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """
         converts a python dict object into a JSON array object
