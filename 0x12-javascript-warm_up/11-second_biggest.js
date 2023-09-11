92f8af30e646311c837d01ccd465cd9cdd825e17
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-09-11 19:20:53 +0300

    add checks

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,11 +3,15 @@
 const cmdArray = process.argv.slice(2);
 const argArray = [];
 
-for (let i = 0; i < cmdArray.length; i++) {
-  argArray.push(Number(cmdArray[i]));
-}
+if (cmdArray.length < 4) {
+  console.log(0);
+} else {
+  for (let i = 0; i < cmdArray.length; i++) {
+    argArray.push(Number(cmdArray[i]));
+  }
 
-const maxNum = Math.max(...argArray);
-const start = argArray.indexOf(maxNum);
-argArray.splice(start, 1);
-console.log(Math.max(...argArray));
+  const maxNum = Math.max(...argArray);
+  const start = argArray.indexOf(maxNum);
+  argArray.splice(start, 1);
+  console.log(Math.max(...argArray));
+}
