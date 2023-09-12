8a5e608ae050b7b34faf5882972b7c41877dbe25
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-09-12 14:50:34 +0300

    add rotate and double functions

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 1 && h > 1) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      /*
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += 'X';
+      }
+      console.log(row);
+      */
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const tempWidth = this.height;
+    this.height = this.width;
+    this.width = tempWidth;
+  }
+
+  double () {
+    this.height = 2 * this.height;
+    this.width = 2 * this.width;
+  }
+}
+
+module.exports = Rectangle;
