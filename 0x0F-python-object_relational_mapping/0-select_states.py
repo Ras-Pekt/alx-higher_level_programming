b131c4dd7f7330607c8d3625eab706907c8150e7
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-09-15 14:19:49 +0300

    fix:select all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
 """lists all states in a database"""
 
-import MySQLdb
-import sys
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
 
 
-MY_HOST = "localhost"
-MY_USER = sys.argv[1]
-MY_PASSWD = sys.argv[2]
-MY_DB = sys.argv[3]
+    MY_HOST = "localhost"
+    MY_USER = sys.argv[1]
+    MY_PASSWD = sys.argv[2]
+    MY_DB = sys.argv[3]
 
-db = MySQLdb.connect(host=MY_HOST, user=MY_USER, passwd=MY_PASSWD, db=MY_DB)
+    db = MySQLdb.connect(host=MY_HOST, user=MY_USER, passwd=MY_PASSWD, db=MY_DB)
 
-cursor = db.cursor()
+    cursor = db.cursor()
 
-cursor.execute("SELECT * FROM states ORDER BY id")
-rows = cursor.fetchall()
+    cursor.execute("SELECT * FROM states ORDER BY id")
+    rows = cursor.fetchall()
 
-for row in rows:
-    print(row)
+    for row in rows:
+        print(row)
