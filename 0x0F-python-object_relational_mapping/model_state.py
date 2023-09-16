f788ba4a7d16b349eb7fe80914ad76d64403c636
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-09-16 14:24:11 +0300

    fix:model states

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -9,7 +9,9 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
+    """mapping class for table states"""
     __tablename__ = "states"
-    id = Column("id", Integer, primary_key=True)
-    name =  Column("name", String(128), nullable=False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
