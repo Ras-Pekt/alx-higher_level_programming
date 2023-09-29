bfd89fd12b9772713f39f3cc36ef39f0310bfdcf
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-09-29 17:33:43 +0300

    post email with requests

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+script that takes in a URL and an email address,
+sends a POST request to the passed URL with the email as a parameter,
+and finally displays the body of the response
+"""
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    url = argv[1]
+    values = {'email': argv[2]}
+
+    response = requests.post(url, data=values)
+    print(response.text)
