76a01b71bc794365a2c8831d94166cd1eef13cfe
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-09-29 18:03:24 +0300

    display error code

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+A script that takes in a URL,
+sends a request to the URL
+and displays the body of the response
+"""
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    url = argv[1]
+    response = requests.get(url)
+    if response.status_code == 200:
+        print(response.text)
+    else:
+        print(f"Error code: {response.status_code}")
