63e35ed8c127265255fd7435c2ac718448cb0d0b
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-09-29 15:50:55 +0300

    display error code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+A script that takes in a URL,
+sends a request to the URL and
+displays the body of the response (decoded in utf-8).
+"""
+
+if __name__ == "__main__":
+    from urllib.request import urlopen
+    from urllib import parse
+    from urllib.error import HTTPError
+    from sys import argv
+
+    try:
+        with urlopen(argv[1]) as response:
+            content = response.read().decode("utf8")
+            print(content)
+    except HTTPError as e:
+        print(f"Error code: {e.code}")
