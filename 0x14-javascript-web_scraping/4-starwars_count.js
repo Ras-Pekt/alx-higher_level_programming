aeb2b2e0df251a04e21fce24277fa7797126d7b7
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-10-24 14:49:03 +0300

    get starwars count

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+// a script that prints the number of movies
+// where the character “Wedge Antilles” is present
+
+const request = require('request');
+
+if (process.argv.length < 3) {
+  process.exit(1);
+}
+
+const url = process.argv[2];
+
+request(url, (err, response, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    const jsonBody = JSON.parse(body);
+    const results = jsonBody.results;
+    let count = 0;
+
+    for (const result of results) {
+      const characters = result.characters;
+
+      for (const character of characters) {
+        if (character === 'https://swapi-api.alx-tools.com/api/people/18/') {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  }
+}
+);
