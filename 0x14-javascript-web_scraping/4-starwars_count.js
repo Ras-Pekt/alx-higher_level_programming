4ef862dd49ca572300ad7cd3ddd6ccb75618521e
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-10-24 15:22:37 +0300

    fix:get starwars count

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -14,24 +14,20 @@ request(url, (err, response, body) => {
   if (err) {
     console.error(err);
   } else {
-    try {
-      const jsonBody = JSON.parse(body);
-      const results = jsonBody.results;
-      let count = 0;
+    const jsonBody = JSON.parse(body);
+    const results = jsonBody.results;
+    let count = 0;
 
-      for (const result of results) {
-        const characters = result.characters;
-  
-        for (const character of characters) {
-          if (character === 'https://swapi-api.alx-tools.com/api/people/18/') {
-            count++;
-          }
+    for (const result of results) {
+      const characters = result.characters;
+
+      for (const character of characters) {
+        if (character === 'https://swapi-api.alx-tools.com/api/people/18/') {
+          count++;
         }
       }
-      console.log(count);
-    } catch (error) {
-      console.log(error);
     }
+    console.log(count);
   }
 }
 );
